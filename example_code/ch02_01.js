c4352fd4310e2444876fb3621d7f8361cf50b1ef
kontais <kontais@aliyun.com>
2017-10-27 23:17:59 +0800

    cha02

---
+++ example_code/ch02_01.js
@@ -0,0 +1,14 @@
+var net = require('net')
+
+var chatServer = net.createServer()
+
+chatServer.on('connection', function(client) {
+  client.write('Hi!\n');
+  client.write('Bye!\n');
+
+  client.end()
+})
+
+chatServer.listen(9000)
+
+// telnet 127.0.0.1 9000
---
+++ example_code/ch02_02.js
@@ -0,0 +1,16 @@
+var net = require('net')
+
+var chatServer = net.createServer()
+
+chatServer.on('connection', function(client) {
+  client.write('Hi!\n');
+
+  client.on('data', function(data) {
+    console.log(data)
+  })
+
+})
+
+chatServer.listen(9000)
+
+// telnet 127.0.0.1 9000
